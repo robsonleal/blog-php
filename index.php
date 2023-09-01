2a538ec27705667d2c3955b638af4e943d679d2a
Robson Leal <robson_leal@outlook.com.br>
2023-09-01 15:18:30 -0300

    Agrupando postagens por mes com exibição decrescente

--- index.php
+++ index.php
@@ -6,29 +6,44 @@ session_start();
 if (isset($_SESSION['resultArray'])) {
   $resultArray = $_SESSION['resultArray'];
 }
-
 ?>
 
 <main>
   <div class="container margin-header">
-    <?php foreach ($resultArray as $index => $postagem) : ?>
-      <?php  ?>
-      <!-- <h1 class="text-center p-2">30 de Agosto de 2023</h1> -->
+    <?php
+    $dataAnterior = new DateTime();
+    $dataAnterior->modify('-1 month');
+
+    foreach ($resultArray as $index => $postagem) {
+
+      $data = new DateTime($postagem['DAT_ALTERACAO']);
+      $dataFormatada = $data->format('F \d\e Y');
+
+      foreach ($months as $en => $ptbr) {
+        $dataFormatada = str_replace($en, $ptbr, $dataFormatada);
+      }
+
+      if ($dataAnterior->format('m') != $data->format('m')) {
+        echo "<h2 class=\"text-center p-2\">$dataFormatada</h2>";
+      }
+
+      $dataAnterior = $data;
+    ?>
+
+
       <div class="row">
         <div class="card bg-light mb-4 col-10 mx-auto">
-         <div class="card-body">
+          <div class="card-body">
             <h5 class="card-title"><a href="#"><?php echo $postagem['TXT_TITULO']; ?></a></h5>
             <h6 class="card-subtitle mb-2 text-body-secondary"><?php echo $postagem['DAT_ALTERACAO']; ?></h6>
             <p class="card-text"><?php echo $postagem['TXT_RESUMO']; ?></p>
-            <?php
-            foreach ($postagem['TXT_TAGS'] as $tags):
+            <?php foreach ($postagem['TXT_TAGS'] as $tags) {
               echo "<span class=\"badge bg-secondary text-uppercase m-1\">$tags</span>";
-            endforeach;
-            ?>
+            } ?>
           </div>
         </div>
       </div>
-    <?php endforeach; ?>
+    <?php } ?>
   </div>
 </main>
 
---
+++ meses_pt_br.php
@@ -0,0 +1,15 @@
+<?php
+$months = [
+    'January' => 'JANEIRO',
+    'February' => 'FEVEREIRO',
+    'March' => 'MARÇO',
+    'April' => 'ABRIL',
+    'May' => 'MAIO',
+    'June' => 'JUNHO',
+    'July' => 'JULHO',
+    'August' => 'AGOSTO',
+    'September' => 'SETEMBRO',
+    'October' => 'OUTUBRO',
+    'November' => 'NOVEMBRO',
+    'December' => 'DEZEMBRO'
+];
