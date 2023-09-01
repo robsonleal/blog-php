35053ea4bed99d3f34b4bcd022c7f4293316a9b0
Robson Leal <robson_leal@outlook.com.br>
2023-09-01 17:34:17 -0300

    Alterando consulta para buscar na nova estrutura do banco, com tabelas dividida para categorias

--- buscar.php
+++ buscar.php
@@ -3,17 +3,30 @@ include_once "conexao.php";
 session_start();
 
 function buscar_todas_postagens_publicadas($conexao) {
+
   $resultArray = array();
-  
-  $sql = "SELECT TXT_TITULO, TXT_RESUMO, TXT_TAGS, DAT_ALTERACAO
-          FROM postagens
-          WHERE PAR_ATIVO = 'S'
-          ORDER BY DAT_ALTERACAO DESC";
+
+  $sql = "SELECT
+            p.TXT_TITULO,
+            p.TXT_RESUMO,
+            p.DAT_ALTERACAO,
+            GROUP_CONCAT(t.TXT_NOME ORDER BY t.TXT_NOME ASC) AS TXT_TAGS
+          FROM
+            desbugando_blog.postagens p
+          LEFT JOIN desbugando_blog.postagens_tags pt ON
+            p.OID_POSTAGEM = pt.OID_POSTAGEM
+          LEFT JOIN desbugando_blog.tags t ON
+            pt.OID_TAG = t.OID_TAG
+          GROUP BY
+            p.OID_POSTAGEM,
+            p.TXT_TITULO
+          ORDER BY
+            p.DAT_ALTERACAO DESC;";
 
   $resultSet = $conexao->query($sql);
 
   while ($row = $resultSet->fetch()) {
-    
+
     $row['TXT_TAGS'] = explode(',', $row['TXT_TAGS']);
 
     array_push($resultArray, $row);
