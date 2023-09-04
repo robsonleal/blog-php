3b192bcc4509ff5e1a426268ca1a137cd3258848
Robson Leal <robson_leal@sicredi.com.br>
2023-09-04 01:58:33 -0300

    [fix] cdn para highlight e deixando apenas min js highlight-badge

--- header.php
+++ header.php
@@ -10,8 +10,8 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
   <!-- highlight  -->
-  <link href="./highlight/styles/vs2015.min.css" rel="stylesheet" />
-  <script src="./highlight/highlight.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/styles/default.min.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/highlight.min.js"></script>
   <!-- Font awesome -->
   <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
   <!--  highlight badge  -->
