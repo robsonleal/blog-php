9a64c5b2581856e9ce1935a2ce48a96a13237675
Robson Leal <robson_leal@outlook.com.br>
2023-09-06 19:32:24 -0300

    [refactor] Colocando formatação da data na classe util

--- src/Util/FormatadorData.php
+++ src/Util/FormatadorData.php
@@ -4,6 +4,32 @@ namespace RobsonLeal\DesbugandoBlog\Util;
 
 class FormatadorData
 {
+  const MESES_PT_BR = [
+    'January' => 'janeiro',
+    'February' => 'fevereiro',
+    'March' => 'março',
+    'April' => 'abril',
+    'May' => 'maio',
+    'June' => 'junho',
+    'July' => 'julho',
+    'August' => 'agosto',
+    'September' => 'setembro',
+    'October' => 'outubro',
+    'November' => 'novembro',
+    'December' => 'dezembro'
+  ];
+
+  public static function formatarDataParaExibicao($data)
+  {
+    $dataFormatada = $data->format('F \d\e Y');
+
+    foreach (FormatadorData::MESES_PT_BR as $en => $ptbr) {
+      $dataFormatada = str_replace($en, $ptbr, $dataFormatada);
+    }
+
+    return $dataFormatada;
+  }
+
   public static function fusoHorarioBr($timestamp)
   {
     $datetime = new \DateTime($timestamp, new \DateTimeZone('UTC'));
